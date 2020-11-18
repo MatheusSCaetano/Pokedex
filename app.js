a93ab036440f0262b9735728cf6b97eee100cc5d
MatheusSCaetano <matheuscaetano260401@gmail.com>
2020-11-18 16:23:09 -0300

    Refatorando algumas funções

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+const getPokemonUrl= id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const generateHTML = pokemons => pokemons = pokemons.reduce((accumulator, pokemon) => {
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+        accumulator += `
+        <li class="card ${types[0]}">
+        <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+        <p class="card-subtitle">${types.join(' | ')}</p>
+        </li>`
+        return accumulator
+    }, '' )
+
+
+const insertPokemonsIntoPage = pokemons => {
+        const ul = document.querySelector('[data-js="pokedex"]')
+        ul.innerHTML = pokemons
+
+    }
+
+
 const fetchPokemon = () => {
-    const getPokemonUrl= id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
     const pokemonPromises = []
 
@@ -8,23 +29,8 @@ const fetchPokemon = () => {
     }
 
     Promise.all(pokemonPromises)
-    .then(pokemons => {
-
-            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
-
-                accumulator += `
-                <li class="card ${types[0]}">
-                <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
-                <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                <p class="card-subtitle">${types.join(' | ')}</p>
-                </li>`
-                return accumulator
-            }, '' )
-
-            const ul = document.querySelector('[data-js="pokedex"]')
-            ul.innerHTML = lisPokemons
-    })
+    .then(generateHTML)
+    .then(insertPokemonsIntoPage)
 }
 
 fetchPokemon()
